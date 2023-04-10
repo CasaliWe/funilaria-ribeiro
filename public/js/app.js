9d9a868a99a3a64514271d508240c32bebc959cc
Weslei Casali <wesleicasali18@gmail.com>
2023-04-10 11:29:57 -0300

    att

--- public/js/app.js
+++ public/js/app.js
@@ -14,3 +14,31 @@ function abrirMenu(){
         document.querySelector('.close-toggler').style.cssText = 'display:none'
     }
 }
+
+
+//TROCAR CAROUSEL BANNER INICAL
+var controllerCarouselBannerInicial = 1
+
+function trocarCarousel(lado){
+     if(lado == 'next'){
+            controllerCarouselBannerInicial == 2 ? controllerCarouselBannerInicial = 1 : controllerCarouselBannerInicial = 2
+            
+            if(controllerCarouselBannerInicial == 1){
+                document.querySelector('._content2-carousel').style.cssText = `display: none;`
+                document.querySelector('._content1-carousel').style.cssText = `display: flex;`
+            }else{
+                document.querySelector('._content2-carousel').style.cssText = `display: flex;`
+                document.querySelector('._content1-carousel').style.cssText = `display: none`
+            }
+     }else{
+        controllerCarouselBannerInicial == 1 ? controllerCarouselBannerInicial = 2 : controllerCarouselBannerInicial = 1
+        
+            if(controllerCarouselBannerInicial == 1){
+                document.querySelector('._content2-carousel').style.cssText = `display: none`
+                document.querySelector('._content1-carousel').style.cssText = `display: flex;`
+            }else{
+                document.querySelector('._content2-carousel').style.cssText = `display: flex`
+                document.querySelector('._content1-carousel').style.cssText = `display: none`
+            }
+     }
+}
